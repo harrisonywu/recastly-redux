6adcd951c7c686f62f92735151ed73d0dd8e0ebc
harrisonywu <harrisonywu@gmail.com>
2019-03-06 19:37:44 -0800

    Implement currentVideo reducer

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,17 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
+var currentVideoReducer = (state = null, action) => {
+  //TODO: define a reducer for the currentVideo field of our state. 
+  /*
+   if the action type is equal to CHANGE_VIDEO "Switch"
+  
+  */
+   switch(action.type){
+       case 'CHANGE_VIDEO':
+         return action.video; // want to change the current video being played to our passed in video key (action.video)
+       default:
+         return state;  
+   }
 };
 
 export default currentVideoReducer;
